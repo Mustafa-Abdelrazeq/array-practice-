e13feeafef3b111698921776aacf0f4f5219d6cb
Mustafa-Abdelrazeq <mustafa.abdelrazeq.mahmoud@gmail.com>
2025-08-02 14:55:19 +0300

    Add files via upload

--- SumRandomArray.py
+++ SumRandomArray.py
@@ -1,4 +1,4 @@
-# Sum All Element Of Array 
+# 1- Sum All Element Of Array 
 
 import numpy as np
 
@@ -12,3 +12,24 @@ def SumAllElementOfArray(Matrix):
     return Sum
 
 print(SumAllElementOfArray(Array))
+print('----------------------------------')
+# ------------------------------------------------------------------------
+# 2- Check To Two Matrices If Sum Equals Each Others 
+    # Count How Many times Taked To Get The Same Sum of Two Matrices
+
+def HowManyTimesToSeeSameResults():
+    Times = 0
+    while True:
+        Array_1 = np.random.randint(1,5 , (3,5))
+        Array_2 = np.random.randint(1,5 , (3,2))
+        
+        if SumAllElementOfArray(Array_1) == SumAllElementOfArray(Array_2):
+            print(Array_1)
+            print()
+            print(Array_2)
+            print()
+            print(f'The Sum Of Each Matrix is : [{SumAllElementOfArray(Array_1)}]')
+            return f'[{Times}] Times.'
+
+        Times+=1
+print(HowManyTimesToSeeSameResults())
